3ef1857f54a7720579d9bdf0b17f64fc225bc900
Pietermikh00 <163128406+Pietermikh00@users.noreply.github.com>
2024-03-13 19:41:02 +0700

    Add files via upload

---
+++ Praktikum1.c
@@ -0,0 +1,96 @@
+Program jarakGLBB
+{Menghitung dan menampilkan jarak S dilayar, S = v0 * t + ½ * (a * t^2)}
+
+Kamus
+v0 : real {kecepatan awal dalam meter per detik}
+t : real {waktu dalam detik}
+a : real {percepatan dalam meter per detik kuadrat}
+S : real {jarak dalam meter}
+
+Algoritma
+input(v0) {input}
+input(t) {input}
+input(a) {input}
+S ← v0 * t + ½ * (a * t^2) {proses, dengan memorisasi}
+output(S) {output}
+
+
+Program jarakPBola
+{Menghitung dan menampilkan jarak y dilayar, y = v0 * t - ½ * (g * t^2)}
+
+Kamus
+v0 : real {kecepatan awal dalam meter per detik}
+t : real {waktu dalam detik}
+g : real {gravitasi dalam meter per detik kuadrat}
+y : real {jarak dalam meter}
+
+Algoritma
+input(v0) {input}
+input(t) {input}
+input(g) {input}
+y ← v0 * t - ½ * (g * t^2) {proses, dengan memorisasi}
+output(y) {output}
+
+
+
+Program gayaSentr
+{Menghitung dan menampilkan Gaya F dilayar, F = m * (v^2 / r)}
+
+Kamus
+m : real {massa dalam kg}
+v : real {kecepatan dalam meter per detik}
+r : real {jari-jari dalam meter}
+F : real {Gaya dalam Newton}
+
+Algoritma
+input(m) {input}
+input(v) {input}
+input(r) {input}
+F ← m * (v^2 / r) {proses, dengan memorisasi}
+output(F) {output}
+
+
+
+Program luasKellLayang
+{Menghitung dan menampilkan luas layang-layang Luas dan keliling layang-layang Kell dilayar, Luas = 1/2 * d1 * d2, Kell = 2 * (s1 + s2)}
+
+Kamus
+s1 : real {sisi satu layang-layang dalam meter}
+s2 : real {sisi dua layang-layang dalam meter}
+d1 : real {diagonal satu layang-layang dalam meter}
+d2 : real {diagonal dua layang-layang dalam meter}
+Luas : real {luas layang-layang dalam meter persegi}
+Kell : real {keliling layang-layang dalam meter}
+
+Algoritma
+input(s1) {input}
+input(s2) {input}
+input(d1) {input}
+input(d2) {input}
+Luas ← 1/2 * d1 * d2 {proses, dengan memorisasi}
+Kell ← 2 * (s1 + s2) {proses, dengan memorisasi}
+output(Luas) {output}
+output(Kell) {output}
+
+
+
+Program luasKellLayang
+{Menghitung dan menampilkan luas layang-layang Luas dan keliling layang-layang Kell dilayar, Luas = 1/2 * d1 * d2, Kell = 2 * (s1 + s2)}
+
+Kamus
+s1 : real {sisi satu layang-layang dalam meter}
+s2 : real {sisi dua layang-layang dalam meter}
+d1 : real {diagonal satu layang-layang dalam meter}
+d2 : real {diagonal dua layang-layang dalam meter}
+Luas : real {luas layang-layang dalam meter persegi}
+Kell : real {keliling layang-layang dalam meter}
+
+Algoritma
+input(s1) {input}
+input(s2) {input}
+input(d1) {input}
+input(d2) {input}
+Luas ← 1/2 * d1 * d2 {proses, dengan memorisasi}
+Kell ← 2 * (s1 + s2) {proses, dengan memorisasi}
+output(Luas) {output}
+output(Kell) {output}
